ffcec80110430d4d2210d60a91a2a5c8b1f7d615
Dom Allen <dominic.allen.canada@gmail.com>
2021-12-06 23:41:58 -0500

    init

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const express = require('express');
+
+const PORT = 8000;
+
+express()
+
+    .use(express.json())
+
+    //endpoints
+
+    .get("/hello", (req, res) => {
+        res.status(200).json({hi: "hi"})
+    })
+
+    .listen(PORT, function() {
+        console.info('🌍 Listening on port ' + PORT);
+    });
